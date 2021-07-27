21ce4ab680f07df4b1bfa2b7663b6b519e322cba
woorively <woorively@gmail.com>
2021-07-27 15:09:56 +0900

    tag page tested

--- blog/models.py
+++ blog/models.py
@@ -25,6 +25,8 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return '/blog/tag/{}/'.format(self.slug)
 
 
 class Post(models.Model):
